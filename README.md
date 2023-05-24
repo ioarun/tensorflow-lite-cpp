ee82d4328895818cf56d5f8101012090dad1ac32
ioarun <arun_kumar271@hotmail.com>
2023-05-24 20:52:00 +1000

    Add tflite check

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tensorflow-lite-cpp
 TFLite build and test
+
+* Host: x86 Ubuntu 20.04
+
+Download bazel from https://github.com/bazelbuild/bazel/releases?q=4.2.1
+
+Copy and rename the download file to /usr/bin/bazel
+
+Download tensorflow from https://github.com/tensorflow/tensorflow/releases/tag/v2.8.4
+
+Remove the content from tflite folder in this repository and replace it with the following.
+
+Create include/ and libs/
+
+Put the following in include/
+
+3rdparty/flatbuffers, tensorflow/lite, tensorflow/core
---
+++ src/build/CMakeFiles/TFLiteCheck.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arun/github/tensorflow-lite-cpp/src/example.cpp" "/home/arun/github/tensorflow-lite-cpp/src/build/CMakeFiles/TFLiteCheck.dir/example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../tflite/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
